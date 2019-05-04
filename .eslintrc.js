5977062944b9ec200dc01a912bf020681617d5bb
Eric Hegnes <eric.hegnes@gmail.com>
2019-05-04 15:50:27 -0400

    Add vuetify

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,33 @@
 module.exports = {
   root: true,
+
   env: {
-    node: true
+    node: true,
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-trailing-spaces': 1,
+    'vue/no-parsing-error': [
+      2,
+      {
+        'unexpected-solidus-in-tag': false,
+      },
+    ],
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        semi: false,
+        trailingComma: 'all',
+      },
+    ],
   },
+
   parserOptions: {
-    parser: "babel-eslint"
-  }
-};
+    parser: 'babel-eslint',
+  },
+
+  extends: ['plugin:vue/recommended', '@vue/prettier'],
+}
